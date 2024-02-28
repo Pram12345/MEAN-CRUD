349955511fb4742775f4472fc37aca435d46c3b3
Ramakant <ramakantprajapati11@gmail.com>
2024-02-28 17:09:11 +0530

    first commit

---
+++ mean_stack/backend/index.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const mongoose = require("mongoose")
+var routers = require('./routes/routes');
+const bodyParser = require("body-parser")
+
+const app = express()
+const cors = require('cors');
+const port = 5000;
+
+const mongodatabaseURL ="mongodb://localhost:27017/sanju";
+
+mongoose.connect(mongodatabaseURL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const connection = mongoose.connection
+
+
+app.listen(port,()=>{
+    console.log("Server is running port" +port);
+
+})
+
+
+connection.once("open",()=>{
+    console.log("MongoDb Connected!!!......")
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(routers);
